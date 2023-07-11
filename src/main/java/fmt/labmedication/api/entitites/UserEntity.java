ebb8db690fb045293eb23dbcdc16cd3d3bd68cbc
Icaro Andrade <iba1987@gmail.com>
2023-07-11 15:34:04 -0300

    feat(user entity): create entity

---
+++ src/main/java/fmt/labmedication/api/entitites/UserEntity.java
@@ -0,0 +1,23 @@
+package fmt.labmedication.api.entitites;
+
+import fmt.labmedication.api.enums.SpecialtyEnum;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserEntity extends PersonEntity {
+    @Column(nullable = false)
+    String crm;
+
+    @Column(nullable = false)
+    SpecialtyEnum specialty;
+
+    @Column(nullable = false)
+    String password;
+}
