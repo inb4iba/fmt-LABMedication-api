ef45ed5584bf0664109f21b171bc86a537759a37
Icaro Andrade <iba1987@gmail.com>
2023-07-12 16:07:04 -0300

    feat(user created): add status code for user created

--- src/main/java/fmt/labmedication/api/controllers/UserController.java
+++ src/main/java/fmt/labmedication/api/controllers/UserController.java
@@ -1,6 +1,8 @@
 package fmt.labmedication.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +25,9 @@ public class UserController {
     private UserMapper mapper;
 
     @PostMapping
-    public ResponseUserDTO registerUser(@RequestBody RegisterUserDTO registerUserDto) {
+    public ResponseEntity<ResponseUserDTO> registerUser(@RequestBody RegisterUserDTO registerUserDto) {
         UserEntity user = mapper.toEntity(registerUserDto);
         ResponseUserDTO responseUserDto = mapper.toDto(this.userService.registerUser(user));
-        return responseUserDto;
+        return new ResponseEntity<ResponseUserDTO>(responseUserDto, HttpStatus.CREATED);
     }
 }
