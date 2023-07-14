901d2719ca51bf7419a6fe648af43f5c5999b6fb
Icaro Andrade <iba1987@gmail.com>
2023-07-14 17:21:24 -0300

    fix(change password): add getter and setter to dto

--- src/main/java/fmt/labmedication/api/dtos/user/PasswordDTO.java
+++ src/main/java/fmt/labmedication/api/dtos/user/PasswordDTO.java
@@ -2,7 +2,11 @@ package fmt.labmedication.api.dtos.user;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class PasswordDTO {
     String oldPassword;
 
