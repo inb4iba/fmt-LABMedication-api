06cfead2154a86aa288ce38ee26165d7ae6e561b
Icaro Andrade <iba1987@gmail.com>
2023-07-18 18:17:06 -0300

    feat(get patients): add get endpoint to get all patients

--- src/main/java/fmt/labmedication/api/services/PatientService.java
+++ src/main/java/fmt/labmedication/api/services/PatientService.java
@@ -1,5 +1,7 @@
 package fmt.labmedication.api.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -29,6 +31,10 @@ public class PatientService {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Paciente não encontrado!"));
         updateAddressDetails(updatedPatient);
         return patientRepository.save(updatedPatient);
+    }
+
+    public List<PatientEntity> getAllPatients() {
+        return patientRepository.findAll();
     }
 
     private void updateAddressDetails(PatientEntity patient) {
