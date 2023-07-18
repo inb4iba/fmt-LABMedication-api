c670df5cf447681dc4a1509672ded51bf06aac6b
Icaro Andrade <iba1987@gmail.com>
2023-07-18 16:57:01 -0300

    fix(register patient): remove args constructor

--- src/main/java/fmt/labmedication/api/entitites/AddressEntity.java
+++ src/main/java/fmt/labmedication/api/entitites/AddressEntity.java
@@ -5,14 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity(name = "addresses")
 @Getter
 @Setter
-@AllArgsConstructor
 public class AddressEntity {
 
     @Id
