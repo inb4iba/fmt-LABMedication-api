6e2d76f6f44d6820e8b9ac0352b210b03d985151
Icaro Andrade <iba1987@gmail.com>
2023-07-18 16:57:49 -0300

    feat(register patient): add post endpoint and register patient

--- src/main/java/fmt/labmedication/api/controllers/PatientController.java
+++ src/main/java/fmt/labmedication/api/controllers/PatientController.java
@@ -1,10 +1,35 @@
 package fmt.labmedication.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import fmt.labmedication.api.dtos.patient.RegisterPatientDTO;
+import fmt.labmedication.api.dtos.patient.ResponsePatientDTO;
+import fmt.labmedication.api.entitites.PatientEntity;
+import fmt.labmedication.api.mappers.PatientMapper;
+import fmt.labmedication.api.services.PatientService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/pacientes")
 public class PatientController {
 
+    @Autowired
+    PatientService patientService;
+
+    @Autowired
+    PatientMapper mapper;
+
+    @PostMapping
+    public ResponseEntity<ResponsePatientDTO> registerPatient(
+            @Valid @RequestBody RegisterPatientDTO registerPatientDTO) {
+        PatientEntity patient = mapper.toEntity(registerPatientDTO);
+        patient = patientService.registerPatient(patient);
+        return new ResponseEntity<ResponsePatientDTO>(mapper.toDto(patient), HttpStatus.CREATED);
+    }
 }
