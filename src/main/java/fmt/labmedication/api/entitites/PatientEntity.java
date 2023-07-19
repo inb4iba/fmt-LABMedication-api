197c151e73599b51ebabef23ceba628e9eb6a0e9
Icaro Andrade <iba1987@gmail.com>
2023-07-19 11:53:40 -0300

    feat(delete patient): create delete endpoint and delete patient

--- src/main/java/fmt/labmedication/api/entitites/PatientEntity.java
+++ src/main/java/fmt/labmedication/api/entitites/PatientEntity.java
@@ -2,7 +2,6 @@ package fmt.labmedication.api.entitites;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToOne;
