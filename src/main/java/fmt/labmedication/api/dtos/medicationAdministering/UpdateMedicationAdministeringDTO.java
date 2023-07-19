340e1116a7e54cad4d77158f88db1b9e86f68a16
Icaro Andrade <iba1987@gmail.com>
2023-07-19 14:43:15 -0300

    feat(update medication administering): add update dto

--- src/main/java/fmt/labmedication/api/dtos/medicationAdministering/UpdateMedicationAdministeringDTO.java
+++ src/main/java/fmt/labmedication/api/dtos/medicationAdministering/UpdateMedicationAdministeringDTO.java
@@ -1,33 +1,24 @@
 package fmt.labmedication.api.dtos.medicationAdministering;
 
-import java.time.LocalDateTime;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import fmt.labmedication.api.enums.MedicationTypeEnum;
 import fmt.labmedication.api.enums.MedicationUnitEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ResponseMedicationAdministeringDTO {
-    Long id;
-
-    String name;
-
-    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
-    LocalDateTime date;
-
+public class UpdateMedicationAdministeringDTO {
+    @NotNull
     MedicationTypeEnum type;
 
-    float amount;
+    @NotNull
+    Float amount;
 
+    @NotNull
     MedicationUnitEnum unit;
 
+    @NotBlank
     String observations;
-
-    Long patientId;
-
-    Long doctorId;
 }
