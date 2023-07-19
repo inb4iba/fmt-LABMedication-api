7fc69e3f1a8c1d2367c24ad7cc45055ed761ec09
Icaro Andrade <iba1987@gmail.com>
2023-07-19 13:12:23 -0300

    fix(register medication administering): add annotations and set date on constructor

--- src/main/java/fmt/labmedication/api/dtos/medicationAdministering/RegisterMedicationAdministeringDTO.java
+++ src/main/java/fmt/labmedication/api/dtos/medicationAdministering/RegisterMedicationAdministeringDTO.java
@@ -6,21 +6,38 @@ import fmt.labmedication.api.entitites.PatientEntity;
 import fmt.labmedication.api.entitites.UserEntity;
 import fmt.labmedication.api.enums.MedicationTypeEnum;
 import fmt.labmedication.api.enums.MedicationUnitEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class RegisterMedicationAdministeringDTO {
+    @NotBlank
     String name;
 
     LocalDateTime date;
 
+    @NotNull
     MedicationTypeEnum type;
 
-    float amount;
+    @NotNull
+    Float amount;
 
+    @NotNull
     MedicationUnitEnum unit;
 
+    @NotBlank
     String observations;
 
+    @NotNull
     PatientEntity patient;
 
+    @NotNull
     UserEntity doctor;
+
+    public RegisterMedicationAdministeringDTO() {
+        date = LocalDateTime.now();
+    }
 }
