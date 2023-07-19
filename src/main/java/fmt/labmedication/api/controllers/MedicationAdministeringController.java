d84cc859417fd49c5fd2abcdc5a7dad9917027af
Icaro Andrade <iba1987@gmail.com>
2023-07-19 12:05:19 -0300

    feat(register medication administering): add controller

---
+++ src/main/java/fmt/labmedication/api/controllers/MedicationAdministeringController.java
@@ -0,0 +1,10 @@
+package fmt.labmedication.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medicamentos")
+public class MedicationAdministeringController {
+
+}
