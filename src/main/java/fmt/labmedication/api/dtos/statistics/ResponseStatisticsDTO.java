5f219c4e798843c43841baf11e34556e921fda98
Icaro Andrade <iba1987@gmail.com>
2023-07-20 14:38:03 -0300

    feat(statistics): create response dto

---
+++ src/main/java/fmt/labmedication/api/dtos/statistics/ResponseStatisticsDTO.java
@@ -0,0 +1,11 @@
+package fmt.labmedication.api.dtos.statistics;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseStatisticsDTO {
+    String name;
+    Integer medicationCount;
+}
