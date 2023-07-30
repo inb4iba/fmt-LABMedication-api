3c12e318dce493d0cad8a7170f41c98844a68f65
Icaro <iba1987@gmail.com>
2023-07-30 12:31:21 -0300

    feat(json-ptbr): add translation to medication

--- src/main/java/fmt/labmedication/api/dtos/medicationAdministering/ResponseMedicationAdministeringDTO.java
+++ src/main/java/fmt/labmedication/api/dtos/medicationAdministering/ResponseMedicationAdministeringDTO.java
@@ -3,6 +3,7 @@ package fmt.labmedication.api.dtos.medicationAdministering;
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import fmt.labmedication.api.enums.MedicationTypeEnum;
 import fmt.labmedication.api.enums.MedicationUnitEnum;
@@ -14,20 +15,28 @@ import lombok.Setter;
 public class ResponseMedicationAdministeringDTO {
     Long id;
 
+    @JsonProperty("nome")
     String name;
 
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    @JsonProperty("data_administracao")
     LocalDateTime date;
 
+    @JsonProperty("tipo_medicamento")
     MedicationTypeEnum type;
 
+    @JsonProperty("quantidade")
     float amount;
 
+    @JsonProperty("unidade_medicamento")
     MedicationUnitEnum unit;
 
+    @JsonProperty("observacoes")
     String observations;
 
+    @JsonProperty("paciente_id")
     Long patientId;
 
+    @JsonProperty("medico_id")
     Long doctorId;
 }
--- src/main/java/fmt/labmedication/api/dtos/medicationAdministering/UpdateMedicationAdministeringDTO.java
+++ src/main/java/fmt/labmedication/api/dtos/medicationAdministering/UpdateMedicationAdministeringDTO.java
@@ -3,6 +3,7 @@ package fmt.labmedication.api.dtos.medicationAdministering;
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import fmt.labmedication.api.enums.MedicationTypeEnum;
 import fmt.labmedication.api.enums.MedicationUnitEnum;
@@ -15,17 +16,22 @@ import lombok.Setter;
 @Setter
 public class UpdateMedicationAdministeringDTO {
     @NotNull
+    @JsonProperty("tipo_medicamento")
     MedicationTypeEnum type;
 
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    @JsonProperty("data_administracao")
     LocalDateTime date;
 
     @NotNull
+    @JsonProperty("quantidade")
     Float amount;
 
     @NotNull
+    @JsonProperty("unidade_medicamento")
     MedicationUnitEnum unit;
 
     @NotBlank
+    @JsonProperty("observacoes")
     String observations;
 }
