d334b7b8ca486e4a1b18244f45b504e75adcea98
Icaro <iba1987@gmail.com>
2023-07-30 12:25:23 -0300

    feat(json-ptbr): add translation to patient

--- src/main/java/fmt/labmedication/api/dtos/patient/RegisterPatientDTO.java
+++ src/main/java/fmt/labmedication/api/dtos/patient/RegisterPatientDTO.java
@@ -3,6 +3,7 @@ package fmt.labmedication.api.dtos.patient;
 import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import fmt.labmedication.api.enums.CivilStateEnum;
 import jakarta.validation.constraints.Email;
@@ -15,12 +16,15 @@ import lombok.Setter;
 @Setter
 public class RegisterPatientDTO {
     @NotBlank
+    @JsonProperty("nome")
     String name;
 
     @NotBlank
+    @JsonProperty("genero")
     String gender;
 
     @JsonFormat(pattern = "dd/MM/yyyy")
+    @JsonProperty("data_de_nascimento")
     LocalDate birthdate;
 
     @NotBlank
@@ -30,31 +34,41 @@ public class RegisterPatientDTO {
     String rg;
 
     @NotNull
+    @JsonProperty("estado_civil")
     CivilStateEnum civilState;
 
     @NotBlank
+    @JsonProperty("telefone")
     String telephone;
 
     @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}")
     String email;
 
     @NotBlank
+    @JsonProperty("naturalidade")
     String nationality;
 
     @NotBlank
+    @JsonProperty("contato_de_emergencia")
     String emergencyContact;
 
+    @JsonProperty("alergias")
     String allergiesList;
 
+    @JsonProperty("cuidados_especificos")
     String specialCaresList;
 
+    @JsonProperty("convenio")
     String healthplan;
 
+    @JsonProperty("numero_carteira_convenio")
     String healthplanNumber;
 
     @JsonFormat(pattern = "dd/MM/yyyy")
+    @JsonProperty("validade_convenio")
     LocalDate healthplanExpireDate;
 
     @NotNull
+    @JsonProperty("endereco_id")
     Long addressId;
 }
