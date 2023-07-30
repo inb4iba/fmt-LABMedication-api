5ba8b4a7dac9f254a731584068c793e2994d5e92
Icaro <iba1987@gmail.com>
2023-07-30 12:33:12 -0300

    feat(json-ptbr): add translation to address

--- src/main/java/fmt/labmedication/api/dtos/address/RegisterAddressDTO.java
+++ src/main/java/fmt/labmedication/api/dtos/address/RegisterAddressDTO.java
@@ -1,5 +1,7 @@
 package fmt.labmedication.api.dtos.address;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -12,21 +14,28 @@ public class RegisterAddressDTO {
     String cep;
 
     @NotBlank
+    @JsonProperty("cidade")
     String city;
 
     @NotBlank
+    @JsonProperty("estado")
     String state;
 
     @NotBlank
+    @JsonProperty("logradouro")
     String street;
 
     @NotNull
+    @JsonProperty("numero")
     Integer number;
 
+    @JsonProperty("complemento")
     String complement;
 
     @NotBlank
+    @JsonProperty("bairro")
     String district;
 
+    @JsonProperty("ponto_referencia")
     String reference;
 }
